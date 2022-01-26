477728bc45de289b3b5905a7d9aa5edfc220368b
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2022-01-26 15:07:12 +0530

    added a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
   imports: [
-    BrowserModule
+  BrowserModule
   ],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe
   ],
   bootstrap: [
     AppComponent
